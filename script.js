6c7216228b069d60718ac3833762d9c026e49cec
pmparitet <pmparitet@mail.ru>
2022-12-13 23:39:17 +0300

    -доб. шаблон карточки(п.2); -доб. карточки в верстку при загрузки стр.(п.2); -доб. лайки (п.5); -изм. стили кнопки лак(заменил img на бекграунд)(п.5);

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// п.1 Работа модальных окон
 let main = document.querySelector(".main");
 let formElement = document.querySelector(".popup");
 let editButton = main.querySelector(".profile__edit-button");
@@ -35,3 +36,57 @@ function formSubmitHandler(evt) {
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", formSubmitHandler);
+
+// п.2 Шесть карточек «из коробки» + п.5 Лайк карточки
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const elements = document.querySelector(".elements");
+
+function addCard(card) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
+
+  cardElement
+    .querySelector(".element__mask-group")
+    .setAttribute("src", card.link);
+  cardElement
+    .querySelector(".element__mask-group")
+    .setAttribute("alt", card.name);
+  cardElement.querySelector(".element__title").textContent = card.name;
+
+  const likeCard = cardElement.querySelector(".element__like-btn");
+  likeCard.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("element__like-btn_active");
+  });
+
+  elements.append(cardElement);
+}
+
+initialCards.forEach((card) => {
+  addCard(card);
+});
