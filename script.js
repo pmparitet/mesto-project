f418ab204f9427dc1133ffb45a9d630a6554bc99
pmparitet <pmparitet@mail.ru>
2022-12-17 00:05:22 +0300

    п.3 Форма добавления карточки п.4 Добавление карточки

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
 // п.1 Работа модальных окон
-let main = document.querySelector(".main");
-let formElement = document.querySelector(".popup");
-let editButton = main.querySelector(".profile__edit-button");
-let closeButton = formElement.querySelector(".popup__close-button");
+const main = document.querySelector(".main");
+// const popupForm = document.querySelectorAll(".popup");
+const editFormElement = document.querySelector("#editForm");
+const addCardFormElement = document.querySelector("#addCardForm");
 
-function openPopup() {
-  let title = main.querySelector(".profile__title");
-  let subtitle = main.querySelector(".profile__subtitle");
-  let nameInput = formElement.querySelector(".popup__field[name='nameInput']");
-  let jobeInput = formElement.querySelector(".popup__field[name='jobeInput']");
+const editButton = main.querySelector(".profile__edit-button");
+const addButton = main.querySelector(".profile__add-button");
+
+const closeEditForm = editFormElement.querySelector(".popup__close-button");
+const closeAddForm = addCardFormElement.querySelector(".popup__close-button");
+
+function openPopup(el) {
+  el.classList.add("popup_opened");
+}
+function closePopup(el) {
+  el.classList.remove("popup_opened");
+}
+
+function editForm() {
+  const title = main.querySelector(".profile__title");
+  const subtitle = main.querySelector(".profile__subtitle");
+  const nameInput = editFormElement.querySelector(
+    ".popup__field[name='nameInput']"
+  );
+  const jobeInput = editFormElement.querySelector(
+    ".popup__field[name='jobeInput']"
+  );
 
   nameInput.value = title.textContent;
   jobeInput.value = subtitle.textContent;
-  formElement.classList.add("popup_opened");
+
+  openPopup(editFormElement);
 }
 
-function closePopup() {
-  formElement.classList.remove("popup_opened");
-}
-
-function formSubmitHandler(evt) {
+function editSubmitHandler(evt) {
   evt.preventDefault();
 
-  let title = main.querySelector(".profile__title");
-  let subtitle = main.querySelector(".profile__subtitle");
-  let nameInput = formElement.querySelector(".popup__field[name='nameInput']");
-  let jobeInput = formElement.querySelector(".popup__field[name='jobeInput']");
+  const title = main.querySelector(".profile__title");
+  const subtitle = main.querySelector(".profile__subtitle");
+  const nameInput = editFormElement.querySelector(
+    ".popup__field[name='nameInput']"
+  );
+  const jobeInput = editFormElement.querySelector(
+    ".popup__field[name='jobeInput']"
+  );
 
   title.textContent = nameInput.value;
   subtitle.textContent = jobeInput.value;
 
-  closePopup();
+  closePopup(editFormElement);
 }
 
-editButton.addEventListener("click", openPopup);
-closeButton.addEventListener("click", closePopup);
-formElement.addEventListener("submit", formSubmitHandler);
+editButton.addEventListener("click", editForm);
+closeEditForm.addEventListener("click", function () {
+  closePopup(editFormElement);
+});
+editFormElement.addEventListener("submit", editSubmitHandler);
 
 // п.2 Шесть карточек «из коробки» + п.5 Лайк карточки
 const initialCards = [
@@ -67,7 +87,7 @@ const initialCards = [
 
 const elements = document.querySelector(".elements");
 
-function addCard(card) {
+function addCard(card, addMethod) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
 
@@ -84,9 +104,44 @@ function addCard(card) {
     evt.target.classList.toggle("element__like-btn_active");
   });
 
-  elements.append(cardElement);
+  switch (addMethod) {
+    case "append":
+      elements.append(cardElement);
+      break;
+    case "prepend":
+      elements.prepend(cardElement);
+      break;
+  }
 }
 
 initialCards.forEach((card) => {
-  addCard(card);
+  addCard(card, "append");
 });
+
+// п.3 Форма добавления карточки + п.4 Добавление карточки
+function addCardForm() {
+  openPopup(addCardFormElement);
+}
+
+function addSubmitHandler(evt) {
+  evt.preventDefault();
+
+  const nameInput = addCardFormElement.querySelector(
+    ".popup__field[name='nameInput']"
+  );
+  const urlInput = addCardFormElement.querySelector(
+    ".popup__field[name='urlInput']"
+  );
+
+  const newCard = { name: nameInput.value, link: urlInput.value };
+  initialCards.unshift(newCard);
+  addCard(newCard, "prepend");
+
+  closePopup(addCardFormElement);
+}
+
+addButton.addEventListener("click", addCardForm);
+closeAddForm.addEventListener("click", function () {
+  closePopup(addCardFormElement);
+});
+addCardFormElement.addEventListener("submit", addSubmitHandler);
